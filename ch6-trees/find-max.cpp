42f99c1c3d7a57abc0bba9ac0e70148174867cb0
sambhav2612 <sambhavjain2612@gmail.com>
2018-06-16 18:21:57 +0530

    Redo Tree ADT
    Signed-off-by: sambhav2612 <sambhavjain2612@gmail.com>

---
+++ ch6-trees/find-max.cpp
@@ -0,0 +1,63 @@
+#include "../includes/tree.h"
+
+int findmax(node *root)
+{
+	int root_value = 0, left_value = 0, right_value = 0, max_value = INT_MIN;
+
+	if (root)
+	{
+		root_value = root->data;
+		left_value = findmax(root->left);
+		right_value = findmax(root->right);
+
+		if (left_value > right_value)
+		{
+			max_value = left_value;
+		}
+		else
+		{
+			max_value = right_value;
+		}
+
+		if (root_value > max_value)
+		{
+			max_value = root_value;
+		}
+	}
+
+	return max_value;
+}
+
+int main()
+{
+	tree obj;
+
+	/**
+	 * Building this tree:
+	 * 
+	 * 			1
+	 * 		   / \
+	 *  	  2   3
+	 *		 /\   /\
+	 *		4  5 6  7
+	 * */
+
+	obj.insert(1);
+	obj.insert(2);
+	obj.insert(3);
+	obj.insert(4);
+	obj.insert(5);
+	obj.insert(6);
+	obj.insert(7);
+
+	cout << endl
+		 << endl
+		 << " Max element is: " << findmax(obj.returnRoot());
+	cout << endl
+		 << " And size of tree is: " << obj.size(obj.returnRoot());
+	cout << endl
+		 << " And height of tree (# levels) is: " << obj.height(obj.returnRoot());
+	cout << endl;
+
+	return 0;
+}
---
+++ ch6-trees/reverse-lca.cpp
@@ -0,0 +1,65 @@
+#include "../includes/tree.h"
+#include <stack>
+
+void reverse(node *root)
+{
+	stack<node *> stk;
+	queue<node *> obj;
+	node *temp;
+
+	if (!root)
+	{
+		return;
+	}
+
+	obj.push(root);
+
+	while (!obj.empty())
+	{
+		temp = obj.front();
+
+		if (temp->left)
+		{
+			obj.push(temp->left);
+		}
+
+		if (temp->right)
+		{
+			obj.push(temp->right);
+		}
+
+		stk.push(temp);
+	}
+
+	while (!stk.empty())
+	{
+		cout << stk.top() << " ";
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	tree obj;
+
+	/**
+	 * Building this tree:
+	 * 
+	 * 			1
+	 * 		   / \
+	 *  	  2   3
+	 *		 /\   /\
+	 *		4  5 6  7
+	 * */
+
+	obj.insert(1);
+	obj.insert(2);
+	obj.insert(3);
+	obj.insert(4);
+	obj.insert(5);
+	obj.insert(6);
+	obj.insert(7);
+
+	reverse(obj.returnRoot());
+
+	return 0;
+}
